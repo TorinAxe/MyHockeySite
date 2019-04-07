ead3cb43d2d245c92df0d29399ba5e9506ba3371
arubtsov <arv.infinity@gmail.com>
2019-04-07 17:17:42 +0300

    [Design] Added Stalin as site moderator

--- templates/module.php
+++ templates/module.php
@@ -1,4 +1,10 @@
-<div id="module_window">
-    <span id="module_window_info"></span>
-    <button id="module_window_btn" class="button btn middle">Закрыть</button>
+<div id="module_wrapper">
+    <div id="module_window">
+        <h3><b>Товарищ, тут тебе сообщение от Вождя Народов!</b></h3>
+        <hr>
+        <span id="module_window_info"></span>
+        <hr>
+        <button id="module_window_btn" class="btn lite_btn large_btn">Прочитано</button>
+    </div>
 </div>
+
