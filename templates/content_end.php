3837327c11c91df8d414d2a122571061ce9928c2
arubtsov <arv.infinity@gmail.com>
2019-04-07 18:17:00 +0300

    [Fix] Footer Changed

--- templates/content_end.php
+++ templates/content_end.php
@@ -1,3 +1,5 @@
 </div> <!-- END of content -->
 <div class="cleaner"></div>
+</div>
+<div id="templatemo_main_bottom"></div>
 </div> <!-- END of main -->
--- templates/login_form.php
+++ templates/login_form.php
@@ -3,13 +3,7 @@
     <form id="login_form">
         <input class="text_edit" id="username" name="username" type="text" value="" required placeholder="Имя пользователя"></br>
         <input class="text_edit" id="password" name="password" type="password" value="" required placeholder="Пароль"></br>
-<<<<<<< HEAD
-        <img   class="captcha"   src = "captcha.php"/><br>
-        <input class="text_edit" name="capcha" required placeholder="Введите текст с картинки"/></br>
-        <input type="button"     id="login_btn" class="btn lite_btn middle align_center" value ="Войти"/>
-=======
         <input type="button"     id="login_btn" class="btn lite_btn middle" value ="Войти"/>
->>>>>>> 7134f223f6d047b7906f1b84fa06c8246f50d7a8
         <p>Еще не зарегистрированы?<a href = register_form.html> Регистрация</a>!</p>
     </form>
 </div>
