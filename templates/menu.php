b1841084d4a2c1661961556f8f59e74381a676df
arubtsov <arv.infinity@gmail.com>
2019-05-03 21:51:58 +0300

    Changed Menu Style

--- templates/menu.php
+++ templates/menu.php
@@ -1,9 +1,22 @@
 <div id="templatemo_menu" class="ddsmoothmenu">
     <ul>
         <li><a class="main_menu_link" href="index.php?page=1" >Главная</a></li>
-        <!--<li><a title="Ответы на популярные вопросы" href="faqs.php">Ответы</a></li>-->
+        <li class="divider"></li>
         <li><a class="main_menu_link" href="about.php">О нас</a></li>
         <li><a class="main_menu_link" href="contact.php">Контакты</a></li>
     </ul>
+    <div class="user_control">
+    <?php
+        if(isset($_SESSION['username'])){
+            echo '<span>Добро пожаловать, '.$_SESSION['username']."</span>
+            <a href=\"shoppingcart.php\"><b>Ваша корзина</b><img src='./images/cart.png'/></a>";
+            echo '<button class="shadow_btn" type="submit" onclick="location.href=\'exit.php\'">Выйти</button>';
+        }
+        else {
+            include "templates/login_form.php";
+            echo '<button onclick="showLoginForm()">Войти</button>';
+        }
+    ?>
+    </div>
     <br style="clear: left" />
 </div> <!-- end of templatemo_menu -->
--- templates/middle.php
+++ templates/middle.php
@@ -1,9 +1,8 @@
 <div id="templatemo_middle">
-    <img align ="right" src="images/123.png"/>
+    <a align="left" href="index.php?page=1"><img src="images/site_logo.png"/></a>
     <h1><?php
         echo $_GET["param"];
 		echo "Приветствую вас в интернет-магазине"
         ?></h1>
     <p>На нашем сайте вы можете купить атрибутику хоккейных команд<p>
-
 </div> <!-- END of middle -->
